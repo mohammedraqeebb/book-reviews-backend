7026c4cf901252248d98be45c8acce28a2785ed7
mohammedraqeebb <mohammedraqeeb9999@gmail.com>
2023-02-10 11:01:46 +0530

    commit

--- src/app.ts
+++ src/app.ts
@@ -22,22 +22,22 @@ const allowedOrigins = [
   'https://book-reviews-taupe.vercel.app',
   'https://book-reviews-mohammedraqeebb.vercel.app',
 ];
-// app.use(function (req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   res.header(
-//     'Access-Control-Allow-Methods',
-//     'GET,PUT,POST,DELETE,UPDATE,OPTIONS'
-//   );
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET,PUT,POST,DELETE,UPDATE,OPTIONS'
+  );
 
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
-//   );
-//   res.header('Access-Control-Max-Age', '10000');
-//   console.log(req.headers.origin);
-//   next();
-// });
+  res.header(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
+  );
+  res.header('Access-Control-Max-Age', '10000');
+  console.log(req.headers.origin);
+  next();
+});
 app.set('trust proxy', 1);
 
 const corsOptions = {
