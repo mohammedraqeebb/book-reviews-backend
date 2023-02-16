931709669dcc3b6795bf68f4d2f44a6afa93e3f5
mohammedraqeebb <mohammedraqeeb9999@gmail.com>
2023-02-16 14:01:44 +0530

    fixed most liked

--- src/controller/book/most-liked.ts
+++ src/controller/book/most-liked.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { Book } from '../../models/book';
 
 export const mostLiked = async (req: Request, res: Response) => {
-  const books = await Book.find({});
+  const books = await Book.find({}).populate('authorIds');
   const topTenBooks = books
     .sort((a, b) => b.likes.length - a.likes.length)
     .slice(0, 10);
