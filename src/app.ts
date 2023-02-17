8f5820f8d5842d4150ecd4c004ccc2d88cafa936
mohammedraqeebb <mohammedraqeeb9999@gmail.com>
2023-02-17 12:31:52 +0530

    removed rate limiter

--- src/app.ts
+++ src/app.ts
@@ -27,7 +27,7 @@ const limiter = rateLimit({
 export const app: Express = express();
 
 app.set('trust proxy', true);
-app.use(limiter);
+// app.use(limiter);
 // app.use(function (req, res, next) {
 //   res.header('Access-Control-Allow-Origin', req.headers.origin);
 //   res.header('Access-Control-Allow-Credentials', 'true');
